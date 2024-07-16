98baccfec83940df6200592114cab23539a1cc54
codeeraa <53138583+codeeraa@users.noreply.github.com>
2024-07-16 19:18:06 +0700

    Update pre_emphasis.h

--- include/pre_emphasis.h
+++ include/pre_emphasis.h
@@ -3,14 +3,13 @@
 
 #include <vector>
 
-// pre-emphasis step
-std::vector<double> preEmphasis(const std::vector<double>& signal, double alpha = 0.97) {
-    std::vector<double> emphasized_signal(signal.size());
-    emphasized_signal[0] = signal[0];  
+std::vector<double> preEmphasis(const std::vector<double>& signal, double alpha) {
+    std::vector<double> emphasizedSignal(signal.size());
+    emphasizedSignal[0] = signal[0];
     for (size_t i = 1; i < signal.size(); ++i) {
-        emphasized_signal[i] = signal[i] - alpha * signal[i - 1];
+        emphasizedSignal[i] = signal[i] - alpha * signal[i - 1];
     }
-    return emphasized_signal;
+    return emphasizedSignal;
 }
 
-#endif 
+#endif
