39cb509e1af5f98d26128f098a65d1f6ab543340
codeeraa <53138583+codeeraa@users.noreply.github.com>
2024-07-26 10:12:19 +0700

    Update hamming_window.h

--- include/hamming_window.h
+++ include/hamming_window.h
@@ -4,19 +4,30 @@
 #include <vector>
 #include <cmath>
 
+#define M_PI           3.14159265358979323846  /* pi */
+
+// function to apply hamming window to frames
 std::vector<std::vector<double>> hammingWindow(const std::vector<std::vector<double>>& frames) {
     int frameSize = frames[0].size();
     std::vector<double> window(frameSize);
+
+    // window coefficients
+    double alpha = 0.54;
+    double beta = 1.0 - alpha;
+    double normalizationFactor = 2 * M_PI / (frameSize - 1);
     for (int i = 0; i < frameSize; ++i) {
-        window[i] = 0.54 - 0.46 * cos(2 * M_PI * i / (frameSize - 1));
+        window[i] = alpha - beta * cos(i * normalizationFactor);
     }
+
+    // apply the Hamming window to each frame
     std::vector<std::vector<double>> windowedFrames(frames.size(), std::vector<double>(frameSize));
     for (size_t i = 0; i < frames.size(); ++i) {
         for (int j = 0; j < frameSize; ++j) {
             windowedFrames[i][j] = frames[i][j] * window[j];
         }
     }
+
     return windowedFrames;
 }
 
-#endif 
+#endif // HAMMING_WINDOW_H
