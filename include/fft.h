3336aca120a19068dec043a1bd6d6cfa124a7e2c
codeeraa <53138583+codeeraa@users.noreply.github.com>
2024-07-26 10:11:31 +0700

    Update fft.h

--- include/fft.h
+++ include/fft.h
@@ -4,21 +4,30 @@
 #include <vector>
 #include <complex>
 #include <fftw3.h>
+#include <algorithm>
 
-std::vector<std::vector<std::complex<double>>> computeFFT(const std::vector<std::vector<double>>& frames) {
+// compute fft function
+std::vector<std::vector<std::complex<double>>> computeFFT(const std::vector<std::vector<double>>& frames, size_t targetSize) {
     int frameSize = frames[0].size();
-    std::vector<std::vector<std::complex<double>>> fftFrames(frames.size(), std::vector<std::complex<double>>(frameSize));
-    fftw_plan plan = fftw_plan_dft_r2c_1d(frameSize, nullptr, nullptr, FFTW_ESTIMATE);
-    std::vector<double> input(frameSize);
-    std::vector<std::complex<double>> output(frameSize);
+    std::vector<std::vector<std::complex<double>>> fftFrames(frames.size(), std::vector<std::complex<double>>(targetSize / 2 + 1));
+    
+    // fftw3 plan
+    fftw_plan plan = fftw_plan_dft_r2c_1d(targetSize, nullptr, nullptr, FFTW_ESTIMATE);
+
+    std::vector<double> input(targetSize);
+    std::vector<std::complex<double>> output(targetSize / 2 + 1);
 
     for (size_t i = 0; i < frames.size(); ++i) {
-        std::copy(frames[i].begin(), frames[i].end(), input.begin());
-        fftw_execute_dft_r2c(plan, input.data(), reinterpret_cast<fftw_complex*>(output.data()));
+        // reflect pad the current frame
+        std::vector<double> paddedFrame = reflectPad(frames[i], targetSize);
+        
+        // perform fft
+        fftw_execute_dft_r2c(plan, paddedFrame.data(), reinterpret_cast<fftw_complex*>(output.data()));
         std::copy(output.begin(), output.end(), fftFrames[i].begin());
     }
+    
     fftw_destroy_plan(plan);
     return fftFrames;
 }
 
-#endif 
+#endif // FFT_H
